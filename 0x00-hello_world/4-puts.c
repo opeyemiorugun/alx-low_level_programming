9bf9a83a521be577ce1ed848d2a04ce6d0e3ef60
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-15 07:07:06 -0700

    Unnecessary

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main() 
+/**
+ * main - This is a description
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char word[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(word);
-	return 0;
+	return (0);
 }
