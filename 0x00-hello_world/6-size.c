5a62900104e9d5b3cd7955ae026be851d6101417
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-15 07:32:06 -0700

    Turn off

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %ul byte(s)\n", sizeof(char));
+	printf("Size of an int: %ul byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ul byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ul byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ul byte(s)\n", sizeof(float));
+	return (0);
