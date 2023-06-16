aea84bcb5c7b0a07622ea89467d5402dd77678aa
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-16 07:15:09 -0700

    Blood, Sweat and Tears

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+
+	for (num = 0; num < 10; ++num)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
+
