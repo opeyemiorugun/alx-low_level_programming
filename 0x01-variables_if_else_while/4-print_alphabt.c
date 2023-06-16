5ca2ce6582e7bbea55433f922141b8b18a6dd7d8
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-16 07:09:11 -0700

    I need u

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@
 int main(void)
 {
 	char letter;
-	int b;
 
 
 	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		if (letter == 'e' || letter == 'q')
-			b = 1;
+			;
 		else
 			putchar(letter);
 	}
