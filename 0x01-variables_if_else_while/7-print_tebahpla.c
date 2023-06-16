7faf8016b53d31efddcbfb8ef766ec69c9f07f1b
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-16 07:32:55 -0700

    IDOL

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+
+	for (letter = 'z'; letter >= 'a'; --letter)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
+
