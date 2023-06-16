968942f99e1a3d4a68a78b07ebe34353aafeffc3
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-16 08:35:27 -0700

    Slavery

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+		putchar(letter);
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
+
