6cd75c660c91cef11997dbf3fa9044375eaecb19
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-20 05:03:15 -0700

    Ghost

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 	while (i <= 10)
 	{
 		for (letter = 'a'; letter <= 'z'; ++letter)
-			putchar(letter);
-		putchar('\n');
+			_putchar(letter);
+		_putchar('\n');
 	++i;
 	}
 }
