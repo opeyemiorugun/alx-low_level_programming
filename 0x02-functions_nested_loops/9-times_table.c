8f5324f56786a90bcf7582380a5edce70e6d2a84
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-20 10:08:20 -0700

    Love you different

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * times_table -prints the 9 timestable
+ *
+ * Return: 9x timetable.
+ */
+void times_table(void)
+{
+	int c, d, e, f;
+
+
+	c = 0;
+	while (c < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			e = c * d;
+			f = e / 10 + 48;
+
+			if (f == '0' && d > 0 && c >= 0)
+				_putchar(' ');
+			else if (d == 0 && c >= 0)
+				;
+			else
+				_putchar(f);
+			_putchar(e % 10 + 48);
+			if (d != 9)
+				_putchar(',');
+			if (d == 9)
+				_putchar('\n');
+			++d;
+		}
+		++c;
+	}
+}
+
+
+
