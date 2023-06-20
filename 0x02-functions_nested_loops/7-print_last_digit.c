a2c4b01aac9b62efcaa0df666a58d65cd94f70f4
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-20 07:24:13 -0700

    Private Landing

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit -Prints last digit
+ *@n: 1st argument
+ *
+ * Return: last digit of the number entered
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
+
