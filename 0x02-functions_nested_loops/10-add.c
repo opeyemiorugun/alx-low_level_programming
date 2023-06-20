8075baafbb1aac169636d7d2deeacb741dd2d339
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-20 10:32:35 -0700

    Lonely

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * add -adds two numbers together
+ * @a: 1st argument
+ * @b: 2nd argument
+ *
+ * Return: the added value of two numbers
+ */
+int add(int a, int b)
+{
+	int c;
+
+
+	c = a + b;
+	return (c);
+}
+
