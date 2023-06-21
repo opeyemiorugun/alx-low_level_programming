7d043be5cc657a2bb0eafabcea1f20c9b1d6f675
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-20 19:43:01 -0700

    Betty that shit...

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,12 +13,26 @@ int main(void)
 	int i;
 	long x = 1, y = 2;
 	long sum = x + y;
+	long x_head, x_tail, y_head, y_tail;
+	long overflow, sum_tail, sum_head;
 
 
 	printf("%lu, %lu, ", x, y);
 	for (i = 3; i <= 98; ++i)
 	{
-		printf("%lu", sum);
+		if (i >= 93)
+		{
+			x_head = x / 100000000;
+			x_tail = x % 100000000;
+			y_head = y / 100000000;
+			y_tail = y % 100000000;
+			overflow = (x_tail + y_tail) / 10;
+			sum_tail = (x_tail + y_tail) % 10;
+			sum_head = (x_head + y_head + overflow);
+			printf("%lu%lu", sum_head, sum_tail);
+		}
+		else
+			printf("%lu", sum);
 		x = y;
 		y = sum;
 		sum = x + y;
