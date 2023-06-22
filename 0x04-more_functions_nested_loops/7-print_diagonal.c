7e2cf8ef6752ae043d8bdf37503bd4f724b9fc5b
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-22 05:54:36 -0700

    Arcade

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal -function that draws a diagonal line on the terminal
+ * 
+ * @n: 1st argument
+ *
+ * Return: the character \
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (i == 1)
+				;
+			else
+			{
+				j = 1;
+				while (j < i)
+				{
+					_putchar('.');
+					j++;
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
