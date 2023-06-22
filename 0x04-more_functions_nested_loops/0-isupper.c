0dedaec6e5507b4f9e9416f03502e44193c344c1
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-22 03:17:39 -0700

    Butter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_times_table -prints the timestable
+ *@n: 1st argument
+ *Return: Timetable.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else 
+		return (0);
+}
+
