d87d4a3f75648f7d993b7aa7c2dc50703466dbfa
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-22 07:46:54 -0700

    Young Blood

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_diagonal -function that draws a diagonal line on the terminal
- * 
+ *
  * @n: 1st argument
  *
  * Return: the character \
@@ -9,26 +9,4 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
-	if (n > 0)
-	{
-		for (i = 1; i <= n; i++)
-		{
-			if (i == 1)
-				;
-			else
-			{
-				j = 1;
-				while (j < i)
-				{
-					_putchar('.');
-					j++;
-				}
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
-	else
-		_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,29 @@
+#include "main.h"
+/**
+ * print_square -function that prints a square, followed by a new line.
+ *
+ * @size: 1st argument
+ *
+ * Return: charater #
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			j = 1;
+			while (j <= size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
 
