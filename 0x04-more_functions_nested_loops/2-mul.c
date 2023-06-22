cbf1cb8cdfc925ad30c03fe1365544d977c67f0a
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-22 03:30:47 -0700

    Dynamite

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * mul -multiplies two integers
+ *@a: 1st argument
+ *@b: 2nd argument
+ *
+ *Return: Timetable.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
