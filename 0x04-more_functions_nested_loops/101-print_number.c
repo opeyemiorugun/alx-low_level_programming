2e4994b0c5bc290b2aa1ba184542af30136a00f0
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-22 16:59:34 -0700

    Clobbered

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,37 +7,31 @@
  */
 void print_number(int n)
 {
-	int x, y;
+	int x = 10, count = 0, reverse = 0;
+	int s, digit, n1;
 
-	if (n >= 10 && n < 100)
+	s = n;
+	n1 = n;
+	do {
+		s /= 10;
+		++count;
+	} while (s != 0);
+	while (n1 != 0)
 	{
-		_putchar(n / 10 + 48);
-		_putchar(n % 10 + 48);
+		reverse = reverse * 10;
+		reverse = reverse + (n1 % 10);
+		n1 = n1 / 10;
 	}
-	else if (n >= 100 && n < 1000)
+	digit = reverse;
+	while (count--)
 	{
-		x = n / 10;
-		_putchar(x / 10 + 48);
-		_putchar(x % 10 + 48);
-		_putchar(n % 10 + 48);
+		if (digit < 0)
+		{
+			digit = digit * -1;
+			_putchar('-);
+		}
+		_putchar(digit % 10 + 48);
+		digit = digit / 10;
 	}
-	else if (n >= 1000 && n < 10000)
-	{
-		x = n / 10;
-		y = x / 10;
-		_putchar(y / 10 + 48);
-		_putchar(y % 10 + 48);
-		_putchar(x % 10 + 48);
-		_putchar(n % 10 + 48);
-	}
-	else if (n < 0)
-	{
-		n = n * -1;
-		_putchar('-');
-		_putchar(n / 10 + 48);
-		_putchar(n % 10 + 48);
-	}
-	else
-		_putchar(n + 48);
 }
 
