6ef41ab06d478725dba1151206c77850405bf295
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-26 09:38:00 -0700

    Saboady

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array -function that prints n elements of an array of
+ * integers, followed by a new line.
+ * @a: Array
+ * @n: size
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
