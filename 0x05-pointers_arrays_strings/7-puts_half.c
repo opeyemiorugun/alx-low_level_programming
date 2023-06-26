0ea3a68db9b47870dd72534dd8b3a1a1cb909112
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-26 09:09:59 -0700

    Skypea

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,3 +25,22 @@ void puts_half(char *str)
 	}
 	_putchar('\n');
 }
+/**
+ * _strlen -function that returns the lenght of a string
+ * @s: 1st argument
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i > -1)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	return (i);
+}
+
