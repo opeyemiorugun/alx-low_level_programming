8c873e19b6e3c9d8299ad8642b9ea8b0ea305d78
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-26 09:48:50 -0700

    Marineford

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcpy -function that prints the copies of the string
+ * @dest: 1st argument
+ * @src: 2nd argument
+ *
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
