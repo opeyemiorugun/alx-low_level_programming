893ec9dcda81247128bb5be6df0522138ff4b107
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-26 05:46:05 -0700

    Enies Lobby

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts -function that prints a string, followed by a new line, to stdout.
+ * @str: 1st argument
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i > -1)
+	{
+		if (s[i] == '\0')
+			break;
+		_putchar(str[i]);
+		i++;
+	}
+}
