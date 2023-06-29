69a32ea304ca583fd3ac0fca04b751ae996bd753
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-29 07:22:38 -0700

    Banana

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strncat -function that concatenates n bytes of one string to
+ * another string
+ * @dest: 1st argument
+ * @src: concatenated
+ * @n: bytes
+ *
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, m = 0, p, r, i, q;
+
+	for (l = 0; dest[l] != '\0'; l++)
+	{
+		p++;
+	}
+	for (q = 0; src[q] != '\0'; q++)
+	{
+		;
+	}
+	n = n - 1;
+	r = l + n;
+	l = 0;
+	for (i = 0; i <= r; i++)
+	{
+		if ((dest[l] == '\0' || i > p) && n >= 0 && m < q)
+		{
+			*(dest + i) = *(src + m);
+			m++;
+			n--;
+		}
+		else
+		{
+			;
+		}
+		l++;
+	}
+	return (dest);
+}
+
