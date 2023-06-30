c26261fcb2d6eda9089c1e9d66b153a749dca4e2
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-06-30 05:34:22 -0700

    Grape

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * cap_string -function that capitalizes all words in a string
+ * @s: String to capitalize
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',')
+		{
+			s[i + 1] = uppercase(s[i + 1]);
+		}
+		else if (s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?')
+		{
+			s[i + 1] = uppercase(s[i + 1]);
+		}
+		else if (s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '}')
+		{
+			s[i + 1] = uppercase(s[i + 1]);
+		}
+		else if (s[i] == '{')
+		{
+			s[i + 1] = uppercase(s[i + 1]);
+		}
+		i++;
+	}
+	return (s);
+}
+/**
+ * uppercase -convert to uppercase
+ * @u: lowercase
+ *
+ * Return: uppercase
+ */
+char uppercase(char u)
+{
+	char *pu;
+
+	pu = &u;
+	if (u >= 97 && u <= 122)
+	{
+		*pu = u - 32;
+	}
+	return (u);
+}
