68e6cac470a70728aa74eced20d1e58034514c57
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-03 15:17:05 -0700

    TECNO

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
