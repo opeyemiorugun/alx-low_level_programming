2a17650c274509673b046e7680bb65ef7a00d700
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-05 10:29:35 -0700

    Good Molecules

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * real_sqrt_recursion -finds the square rooot of a number
+ * @n: number
+ * @c: potentioal root
+ *
+ * Return: the square root
+ */
+int real_sqrt_recursion(int n, int c)
+{
+	if (((c * c) == n) && (c < n / 2))
+	{
+		return (c);
+	}
+	else if (c == n / 2)
+	{
+		return (-1);
+	}
+	real_sqrt_recursion(n, c + 1);
+}
+/**
+ * _sqrt_recursion -displays the square root of a function.
+ * @n: number
+ *
+ * Return: square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (real_sqrt_recursion(n, 2));
+}
