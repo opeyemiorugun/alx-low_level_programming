b78fe9620de8043370e7595352c04140fca3e909
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-10 02:31:24 -0700

    Python

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_array -create an array of chars, and initializes it with a
+ * specific char.
+ * @size: size in bytes that is to be allocated
+ * @char: charcater to be initialized
+ * Return: NULL if size = 0, a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *) malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (size--)
+	{
+		ptr[size] = c;
+	}
+	return (ptr);
+}
