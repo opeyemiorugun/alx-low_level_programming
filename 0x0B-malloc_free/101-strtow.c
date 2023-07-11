165478eee59fbb6552a3d8532d8419289b675b10
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-11 08:38:00 -0700

    Perl

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow -splits a string into words
+ * @str: 1st argument
+ * Return: a pointer to an array of strings(words)
+ */
+char **strtow(char *str)
+
+}
