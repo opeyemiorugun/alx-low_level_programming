b53b858b250b4f5b23089674a5ede87cc051647f
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-11 07:04:11 -0700

    Swift

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr -concatenates all arguments of your program
+ * @ac: length of the program's argument
+ * @av: array of program's arguments
+ * Return: a character.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	p = (char *) malloc(ac * sizeof(av));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		while (*(*av + j) != '\0')
+		{
+			p[j] = *(*av + j);
+			j++;
+		}
+		p[j] = '\n';
+		j++;
+	}
+	return (p);
+}
