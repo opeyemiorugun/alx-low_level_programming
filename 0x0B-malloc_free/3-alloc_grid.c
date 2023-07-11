9155e6a1e8b28b80d9e0c5fd559c6ad080facb3e
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-11 04:32:52 -0700

    C++

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,34 +9,47 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int **array;
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	array = malloc(sizeof(*array) * width);
-
-	if (array == NULL)
-	{
-		free(array);
-		return (NULL);
-	}
-
-	for (i = 0; i < width; i++)
-	{
-		array[i] = malloc(sizeof(*array[i]) * height);
-		if (array[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-				free(array[j]);
-		}
-	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-			array[i][j] = 0;
-	}
-	return (array);
+        /*ROW TO COLUMN*/
+        int i, j;
+        int **array;
+        /*Change variable names for ease*/
+        int column, row;
+        column = width;
+        row = height;
+        
+        if (row <= 0 || column <= 0)
+        {
+                return (NULL);     
+        }
+        /*STEP 1: Dynamically Allocate an Array of Rows Pointers. 
+        *First, we dynamically allocate an array of pointers, 
+        *where each of these pointers will later point to a row in the 2D array
+	*/
+        array = (int **) malloc(row * sizeof(int *));
+        
+        /*STEP 2: Dynamically Allocate Each Row. */
+        /*Second, we dynamically allocate 1D arrays each corresponding to a row.*/
+        for (i = 0; i < row; i++)
+        {
+                array[i] = (int *) malloc(column * sizeof(int)); 
+		/*ATTEMPT TO FREE ARRAY*/
+                if (array[i] == NULL)
+                {
+                        free(array[i]);
+                        free(array);
+                        return (NULL);
+                }
+        }
+        
+        /*STEP 3: Assign a value to each element*/
+        for (i = 0; i < row; i++)
+        {
+                for (j = 0; j < column; j++)
+                {
+                        /*array[row][column]*/
+                        array[i][j] = 0;
+                }
+        }
+        
+        return (array);
 }
-
