e10335be3145edd5eba83119007d991e8371eaee
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-13 03:54:31 -0700

    Uchicha Sasuke

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc -a copy of the calloc function using malloc
+ * @nmemb: no. of elements
+ * @size: size of each element
+ * Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int j;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < (nmem * size); j++)
+	{
+		*(p + j) = 0;
+	}
+	return (p);
+}
