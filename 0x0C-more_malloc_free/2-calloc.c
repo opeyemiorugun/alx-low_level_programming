901cfd52074ab0d64c9c40bd7bd1f8da7ce430ee
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-07-13 05:30:48 -0700

    Jiraiya

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (j = 0; j < (nmem * size); j++)
+	for (j = 0; j < (nmemb * size); j++)
 	{
 		*(p + j) = 0;
 	}
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr =  mallloc(sizeof(int) * (max - min + 1));
+	arr =  malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 
