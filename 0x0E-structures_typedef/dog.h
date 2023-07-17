aa391a9a6b30bfe886e6e698b1aeb060f8094e08
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-17 05:24:14 -0700

    Boss Mode

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dogstruct
+#define dogstruct
+/**
+ * struct dog -short description
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
