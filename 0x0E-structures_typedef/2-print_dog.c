6316bab64e4f398bf5b14749d53bfd4cdc655113
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-17 06:44:36 -0700

    Annoying network

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * print_dog -function that prints a struct dog
+ * @d: pointer to struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		(d->name == NULL) ? (printf("(nil)\n")) : (printf("%s\n", d->name));
+		printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		(d->owner == NULL) ? (printf("(nil)\n")) : (printf("%s\n", d->owner));
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-/**
- * parameters -typedef for struct dog
- */
-typedef struct dog parameter;
-	void init_dog(struct dog *d, char *name, float age, char *owner);
+}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 #endif
