f71282e37b4d07b70379d203b267fb1ce92f7a7d
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-20 10:45:19 -0700

    Kami

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,41 +6,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0, flag;
-	char *string, *separator = ", ";
+	unsigned int i = 0, flag, j = 0;
+	char *string, *separator = ", ", copy[] = "cifs";
 	va_list list_all;
 
 	va_start(list_all, format);
-
 	while (format && format[i])
 	{
-		if (i > 0 && flag == 1)
-			printf("%s", separator);
+		j = 0;
+		while (copy[j] != '\0')
+		{
+			if (format[i] == copy[j] && flag && i > 0)
+			{
+				printf("%s", separator);
+				break;
+			}
+			j++;
+		}
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(list_all, int));
-			flag = 1;
+			printf("%c", va_arg(list_all, int)), flag = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(list_all, int));
-			flag = 1;
+			printf("%d", va_arg(list_all, int)), flag = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(list_all, double));
-			flag = 1;
+			printf("%f", va_arg(list_all, double)), flag = 1;
 			break;
 		case 's':
 			string = va_arg(list_all, char *);
 			if (!string)
 				string = ("(nil)");
-			printf("%s", string);
-			flag = 1;
+			printf("%s", string), flag = 1;
 			break;
 		default:
 			break;
-		}
-	       i++;
+		} i++;
 	}
 	printf("\n");
 	va_end(list_all);
