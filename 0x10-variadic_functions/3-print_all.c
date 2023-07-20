62984dc5d5a7b2f7628b42019ccb03dfb98ecec4
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-20 09:55:37 -0700

    K

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,11 @@ void print_all(const char * const format, ...)
 	va_list list_all;
 
 	va_start(list_all, format);
-
+	if (!format)
+	{
+		printf("\n");
+		exit(34);
+	}
 	while (format && format[i])
 	{
 		if (i > 0 && flag == 1)
