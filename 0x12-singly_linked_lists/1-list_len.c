cade9c9688eae8147f6f6ff60cf9211cd7efcf28
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-27 09:19:33 -0700

    Work

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len -returns the number of elements in a linked list_t.
+ *
+ * @h: linked list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+	const list_t *temp;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (i);
+}
