5b119d04bb1086772f6adfd5043c421aef3c6c2d
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-27 08:50:11 -0700

    Without clockwork

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list -prints all elements of a list_t list.
+ *
+ * @h: list_t list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+	int i = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
