3c9b63137c69c5d9521f49334ef3a3234630cdff
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-28 05:11:15 -0700

    SUsuME

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * tortoise -function that runs before main
+ *
+ */
+void tortoise()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
