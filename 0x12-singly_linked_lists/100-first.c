3e1719e9064780d01eb54e20477e31db1481317a
opeyemiorugun <opeyemiorugun@stu.cu.edu.ng>
2023-07-28 05:34:17 -0700

    Laugh...Waratte

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * tortoise -function that runs before main
  *
  */
-void tortoise()
+void tortoise(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
