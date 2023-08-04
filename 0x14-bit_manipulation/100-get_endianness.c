f01f7859a840a32f6b1556d3296aa8f466979b1a
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-08-04 04:13:00 -0700

    MAP

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_endianness -function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *p_num;
+
+	p_num = (char *)&num;
+
+	if (*p_num == 1)
+		return (1);
+	return (0);
+}
