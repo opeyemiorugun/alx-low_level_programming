eb2b8427933d2f301333a03d30381e721bad1516
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-08-04 02:36:57 -0700

    TOMORROW

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	mask = (1 << index);
+	mask = ~(1 << index);
 
 	*n = *n & mask;
 
