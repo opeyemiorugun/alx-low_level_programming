d54e3003370307af062f459e6156e8f6f73d8b7e
opeyemiorugun <opeyemi.orugun@stu.cu.edu.ng>
2023-08-07 11:40:47 -0700

    Gear 5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,19 +11,15 @@
 int main(int argc, char *argv[])
 {
 	int bytes_written, fd1, fd2, bytes_read, sz;
-	char *file_to, *file_from;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	file_from = argv[1];
-	file_to = argv[2];
-
-	fd1 = open(file_from, O_RDONLY);
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDOUT_FILENO, R_ERROR, argv[1]), exit(98);
-	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fd2 == -1)
 		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
